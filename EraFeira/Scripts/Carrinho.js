1727c01545539f78e3b3ba83f981e3a93312cac3
taina.aparecida.passos <taina.aparecida.passos@outlook.com>
2019-11-04 20:24:34 -0300

    Cookie do carrinho
    adição e subtração de cada produto, e a soma da cesta

---
+++ EraFeira/Scripts/Carrinho.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+
+    //-- Clique para adicionar ou retirar
+    $(".btn-menos").on("click", function () {
+        var id = "input_"+$(this).attr("produtoID");
+        var now = $(id).val();
+        if ($.isNumeric(now)) {
+            if (parseInt(now) - 1 >= 0) { now--; }
+            $(id).val(now);
+        } else {
+            $(id).val("0");
+        }
+    })
+    $(".btn-mais").on("click", function () {
+        var id = "input_" + $(this).attr("produtoID");
+        var now = $(id).val();
+        if ($.isNumeric(now)) {
+            $(id).val(parseInt(now) + 1);
+        } else {
+            $(id).val("0");
+        }
+    })
+})
---
+++ EraFeira/ViewModels/EntregasViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations; // biblioteca importada
+using System.Linq;
+using System.Web;
+
+namespace EraFeira.ViewModels
+{
+    public class EntregasViewModel
+    {
+        public int QntCesta { get; set; }
+
+        public int Intervalo { get; set; }
+
+        //[EnumDataType(typeof(TipoCesta))]
+        [Display(Name = "")]
+        public TipoCesta Ass_tipo_cesta { get; set; }
+        public enum TipoCesta { igual, diferente }
+    }
+}
